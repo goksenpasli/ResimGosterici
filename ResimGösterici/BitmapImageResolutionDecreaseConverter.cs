6ded6e35a48102c75332256e8dd4b55f47767605
goksenpasli <gpasli@gmail.com>
2021-03-20 16:41:23 +0300

    Added Converter

---
+++ ResimGösterici/BitmapImageResolutionDecreaseConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace ResimGösterici
+{
+    public class BitmapImageResolutionDecreaseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Uri uri)
+            {
+                BitmapImage bi = new();
+                bi.BeginInit();
+                bi.UriSource = uri;
+                bi.DecodePixelHeight= (int)System.Windows.SystemParameters.PrimaryScreenHeight;
+                bi.EndInit();
+                bi.Freeze();
+                return bi;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
