31a3b5644a595830b920a3e38eebd49203e625a2
goksenpasli <gpasli@gmail.com>
2021-03-20 22:38:37 +0300

    Added Image Fake Shadow

---
+++ ResimGösterici/ShadowedImage.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ResimGösterici
+{
+    internal class ShadowedImage : Image
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            dc.DrawRectangle(new SolidColorBrush(Color.FromArgb(70,128,128,128)), null, new Rect(new Point(2.5, 2.5), new Size(ActualWidth, ActualHeight)));
+            base.OnRender(dc);
+        }
+    }
+}
