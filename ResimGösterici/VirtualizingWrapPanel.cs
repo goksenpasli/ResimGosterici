3dc7a7d1475fe1bdb3efa844ea7eaa29a6ad49a8
goksenpasli <gpasli@gmail.com>
2021-03-21 12:09:13 +0300

    Wrap Panel Code Opt

--- ResimGösterici/VirtualizingWrapPanel.cs
+++ ResimGösterici/VirtualizingWrapPanel.cs
@@ -10,52 +10,47 @@ using System.Windows.Media;
 
 namespace ResimGösterici
 {
-    public class VirtualizingUniformSizeWrapPanel : VirtualizingPanel, IScrollInfo
+    public class VirtualizingUniformSizeWrapPanel : VirtualizingPanel,
+    IScrollInfo
     {
         #region ItemSize
 
         #region ItemWidth
 
-        public static readonly DependencyProperty ItemWidthProperty =
-            DependencyProperty.Register(
-                "ItemWidth",
-                typeof(double),
-                typeof(VirtualizingUniformSizeWrapPanel),
-                new FrameworkPropertyMetadata(
-                    double.NaN,
-                    FrameworkPropertyMetadataOptions.AffectsMeasure
-                ),
-                new ValidateValueCallback(IsWidthHeightValid)
-            );
+        public static readonly DependencyProperty ItemWidthProperty = DependencyProperty.Register("ItemWidth", typeof(double), typeof(VirtualizingUniformSizeWrapPanel), new FrameworkPropertyMetadata(
+        double.NaN, FrameworkPropertyMetadataOptions.AffectsMeasure), new ValidateValueCallback(IsWidthHeightValid));
 
         [TypeConverter(typeof(LengthConverter))]
         public double ItemWidth
         {
-            get { return (double)GetValue(ItemWidthProperty); }
-            set { SetValue(ItemWidthProperty, value); }
+            get
+            {
+                return (double)GetValue(ItemWidthProperty);
+            }
+            set
+            {
+                SetValue(ItemWidthProperty, value);
+            }
         }
 
         #endregion
 
         #region ItemHeight
 
-        public static readonly DependencyProperty ItemHeightProperty =
-            DependencyProperty.Register(
-                "ItemHeight",
-                typeof(double),
-                typeof(VirtualizingUniformSizeWrapPanel),
-                new FrameworkPropertyMetadata(
-                    double.NaN,
-                    FrameworkPropertyMetadataOptions.AffectsMeasure
-                ),
-                new ValidateValueCallback(IsWidthHeightValid)
-            );
+        public static readonly DependencyProperty ItemHeightProperty = DependencyProperty.Register("ItemHeight", typeof(double), typeof(VirtualizingUniformSizeWrapPanel), new FrameworkPropertyMetadata(
+        double.NaN, FrameworkPropertyMetadataOptions.AffectsMeasure), new ValidateValueCallback(IsWidthHeightValid));
 
         [TypeConverter(typeof(LengthConverter))]
         public double ItemHeight
         {
-            get { return (double)GetValue(ItemHeightProperty); }
-            set { SetValue(ItemHeightProperty, value); }
+            get
+            {
+                return (double)GetValue(ItemHeightProperty);
+            }
+            set
+            {
+                SetValue(ItemHeightProperty, value);
+            }
         }
 
         #endregion
@@ -72,26 +67,27 @@ namespace ResimGösterici
 
         #region Orientation
 
-        public static readonly DependencyProperty OrientationProperty =
-            WrapPanel.OrientationProperty.AddOwner(
-                typeof(VirtualizingUniformSizeWrapPanel),
-                new FrameworkPropertyMetadata(
-                    Orientation.Horizontal,
-                    FrameworkPropertyMetadataOptions.AffectsMeasure,
-                    new PropertyChangedCallback(OnOrientationChanged)
-                )
-            );
+        public static readonly DependencyProperty OrientationProperty = WrapPanel.OrientationProperty.AddOwner(
+        typeof(VirtualizingUniformSizeWrapPanel), new FrameworkPropertyMetadata(
+        Orientation.Horizontal, FrameworkPropertyMetadataOptions.AffectsMeasure, new PropertyChangedCallback(OnOrientationChanged)));
 
         public Orientation Orientation
         {
-            get { return (Orientation)GetValue(OrientationProperty); }
-            set { SetValue(OrientationProperty, value); }
+            get
+            {
+                return (Orientation)GetValue(OrientationProperty);
+            }
+            set
+            {
+                SetValue(OrientationProperty, value);
+            }
         }
 
         private static void OnOrientationChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             VirtualizingUniformSizeWrapPanel panel = d as VirtualizingUniformSizeWrapPanel;
-            panel.offset = default;
+            panel.offset =
+        default;
             panel.InvalidateMeasure();
         }
 
@@ -99,9 +95,14 @@ namespace ResimGösterici
 
         #region MeasureOverride, ArrangeOverride
 
-        private Size PrevViewPortSize { set; get; }
+        private Size PrevViewPortSize
+        {
+            set;
+            get;
+        }
 
-        private readonly ConcurrentDictionary<int, Rect> containerLayouts = new();
+        private readonly ConcurrentDictionary<int,
+        Rect> containerLayouts = new();
 
         private double GetLogicalWidth(Size size)
         {
@@ -131,25 +132,27 @@ namespace ResimGösterici
         {
             bool isHorizontal = Orientation == Orientation.Horizontal;
 
-            Size maxSize = default(Size);
-            if (GetLogicalWidth(availableSize) < 1) return maxSize;
+            Size maxSize = default;
+            if (GetLogicalWidth(availableSize) < 1)
+            {
+                return maxSize;
+            }
 
             bool isChaigeViewPortSize = PrevViewPortSize != availableSize;
             if (isChaigeViewPortSize)
             {
-                Console.WriteLine("ViewPoirt:" + availableSize);
                 containerLayouts.Clear();
             }
 
-            int prevColumCount = Math.Max(1, Convert.ToInt32(GetLogicalWidth(PrevViewPortSize)) / Convert.ToInt32(GetLogicalWidth(prevSize)));
+            int prevColumCount = Math.Max(1, (int)(GetLogicalWidth(PrevViewPortSize)) / (int)(GetLogicalWidth(prevSize)));
 
-            int vy = Convert.ToInt32(offset.Y) / Convert.ToInt32(prevSize.Height);
-            int vx = Convert.ToInt32(GetLogicalX(offset)) / Convert.ToInt32(prevSize.Width);
+            int vy = (int)(offset.Y) / (int)(prevSize.Height);
+            int vx = (int)(GetLogicalX(offset)) / (int)(prevSize.Width);
             int first = vx + vy * prevColumCount;
 
-            double vey = (Convert.ToInt32(GetLogicalY(offset)) + GetLogicalHeight(PrevViewPortSize)) / Convert.ToInt32(GetLogicalHeight(prevSize)) + 2;
-            double vex = (Convert.ToInt32(GetLogicalX(offset)) + GetLogicalWidth(PrevViewPortSize)) / Convert.ToInt32(GetLogicalWidth(prevSize)) + 1;
-            int end = Convert.ToInt32(vex + vey * prevColumCount) + 1;
+            double vey = ((int)(GetLogicalY(offset)) + GetLogicalHeight(PrevViewPortSize)) / (int)(GetLogicalHeight(prevSize)) + 2;
+            double vex = ((int)(GetLogicalX(offset)) + GetLogicalWidth(PrevViewPortSize)) / (int)(GetLogicalWidth(prevSize)) + 1;
+            int end = (int)(vex + vey * prevColumCount) + 1;
 
             int childrenCount = 0;
             ItemsControl itemsControl = ItemsControl.GetItemsOwner(this);
@@ -157,7 +160,10 @@ namespace ResimGösterici
             {
                 childrenCount = itemsControl.Items.Count;
             }
-            if (childrenCount < 1) return maxSize;
+            if (childrenCount < 1)
+            {
+                return maxSize;
+            }
 
             if (isChaigeViewPortSize)
             {
@@ -175,9 +181,8 @@ namespace ResimGösterici
 
             int firstIndex = Math.Max(0, first);
             int endIndex = Math.Min(end, childrenCount);
-            Console.WriteLine("Index:{0},{1}", first, end);
 
-            int columCount = Math.Max(1, Convert.ToInt32(GetLogicalWidth(availableSize)) / Convert.ToInt32(GetLogicalWidth(prevSize)));
+            int columCount = Math.Max(1, (int)(GetLogicalWidth(availableSize)) / (int)(GetLogicalWidth(prevSize)));
 
             Point adjustViewPortOffset = offset;
             if (isChaigeViewPortSize)
@@ -221,7 +226,10 @@ namespace ResimGösterici
                     continue;
                 }
 
-                if (continium) break;
+                if (continium)
+                {
+                    break;
+                }
             }
 
             using (ChildGenerator generator = new(this, firstIndex, endIndex))
@@ -250,7 +258,6 @@ namespace ResimGösterici
                         continium = true;
                         continue;
                     }
-
                 }
                 generator.CleanupChildren();
             }
@@ -318,13 +325,18 @@ namespace ResimGösterici
                 }
 
                 if (first < int.MaxValue)
+                {
                     firstGeneratedIndex = first;
+                }
                 else
+                {
                     firstGeneratedIndex = -1;
+                }
 
-                if (last >= 0) lastGeneratedIndex = last;
-
-                Console.WriteLine("F{0}:L{1}", firstGeneratedIndex, lastGeneratedIndex);
+                if (last >= 0)
+                {
+                    lastGeneratedIndex = last;
+                }
             }
 
             public ChildGenerator(VirtualizingUniformSizeWrapPanel owner, int firstIndex, int endIndex)
@@ -335,8 +347,6 @@ namespace ResimGösterici
 
                 firstGeneratedIndex = firstIndex;
                 lastGeneratedIndex = endIndex;
-
-                Console.WriteLine("F{0}:L{1}", firstGeneratedIndex, lastGeneratedIndex);
             }
 
             ~ChildGenerator()
@@ -372,13 +382,17 @@ namespace ResimGösterici
             public UIElement GetChild(int index)
             {
                 if (generator == null)
+                {
                     return owner.InternalChildren[index];
+                }
 
                 if (generator is ItemContainerGenerator itemGenerator)
                 {
                     DependencyObject item = itemGenerator.ContainerFromIndex(index);
                     if (item != null)
+                    {
                         return item as UIElement;
+                    }
                 }
                 return null;
             }
@@ -386,25 +400,35 @@ namespace ResimGösterici
             public UIElement GetOrCreateChild(int index)
             {
                 if (generator == null)
+                {
                     return owner.InternalChildren[index];
+                }
 
                 if (generator is ItemContainerGenerator itemGenerator)
                 {
                     DependencyObject item = itemGenerator.ContainerFromIndex(index);
                     if (item != null)
+                    {
                         return item as UIElement;
+                    }
                 }
 
                 if (generatorTracker == null)
+                {
                     BeginGenerate(index);
+                }
 
                 UIElement child = generator.GenerateNext(out bool newlyRealized) as UIElement;
                 if (newlyRealized)
                 {
                     if (currentGenerateIndex >= owner.InternalChildren.Count)
+                    {
                         owner.AddInternalChild(child);
+                    }
                     else
+                    {
                         owner.InsertInternalChild(currentGenerateIndex, child);
+                    }
 
                     generator.PrepareItemContainer(child);
                 }
@@ -421,7 +445,9 @@ namespace ResimGösterici
             public void CleanupChildren()
             {
                 if (generator == null)
+                {
                     return;
+                }
 
                 UIElementCollection children = owner.InternalChildren;
 
@@ -454,7 +480,7 @@ namespace ResimGösterici
                 }
             }
 
-            return finalSize;      
+            return finalSize;
         }
 
         #endregion
@@ -487,12 +513,18 @@ namespace ResimGösterici
 
         public double ExtentHeight
         {
-            get { return extent.Height; }
+            get
+            {
+                return extent.Height;
+            }
         }
 
         public double ExtentWidth
         {
-            get { return extent.Width; }
+            get
+            {
+                return extent.Width;
+            }
         }
 
         #endregion Extent
@@ -503,12 +535,18 @@ namespace ResimGösterici
 
         public double ViewportHeight
         {
-            get { return viewport.Height; }
+            get
+            {
+                return viewport.Height;
+            }
         }
 
         public double ViewportWidth
         {
-            get { return viewport.Width; }
+            get
+            {
+                return viewport.Width;
+            }
         }
 
         #endregion
@@ -519,26 +557,44 @@ namespace ResimGösterici
 
         public double HorizontalOffset
         {
-            get { return offset.X; }
+            get
+            {
+                return offset.X;
+            }
         }
 
         public double VerticalOffset
         {
-            get { return offset.Y; }
+            get
+            {
+                return offset.Y;
+            }
         }
 
         #endregion
 
         #region ScrollOwner
-        public ScrollViewer ScrollOwner { get; set; }
+        public ScrollViewer ScrollOwner
+        {
+            get;
+            set;
+        }
         #endregion
 
         #region CanHorizontallyScroll
-        public bool CanHorizontallyScroll { get; set; }
+        public bool CanHorizontallyScroll
+        {
+            get;
+            set;
+        }
         #endregion
 
         #region CanVerticallyScroll
-        public bool CanVerticallyScroll { get; set; }
+        public bool CanVerticallyScroll
+        {
+            get;
+            set;
+        }
         #endregion
 
         #region LineUp
@@ -637,22 +693,36 @@ namespace ResimGösterici
             }
 
             if (idx < 0)
+            {
                 return Rect.Empty;
+            }
 
             if (!containerLayouts.ContainsKey(idx))
+            {
                 return Rect.Empty;
+            }
 
             Rect layout = containerLayouts[idx];
 
             if (HorizontalOffset + ViewportWidth < layout.X + layout.Width)
+            {
                 SetHorizontalOffset(layout.X + layout.Width - ViewportWidth);
+            }
+
             if (layout.X < HorizontalOffset)
+            {
                 SetHorizontalOffset(layout.X);
+            }
 
             if (VerticalOffset + ViewportHeight < layout.Y + layout.Height)
+            {
                 SetVerticalOffset(layout.Y + layout.Height - ViewportHeight);
+            }
+
             if (layout.Y < VerticalOffset)
+            {
                 SetVerticalOffset(layout.Y);
+            }
 
             layout.Width = Math.Min(ViewportWidth, layout.Width);
             layout.Height = Math.Min(ViewportHeight, layout.Height);
@@ -671,7 +741,9 @@ namespace ResimGösterici
             else
             {
                 if (offset + ViewportWidth >= ExtentWidth)
+                {
                     offset = ExtentWidth - ViewportWidth;
+                }
             }
 
             this.offset.X = offset;
@@ -692,7 +764,9 @@ namespace ResimGösterici
             else
             {
                 if (offset + ViewportHeight >= ExtentHeight)
+                {
                     offset = ExtentHeight - ViewportHeight;
+                }
             }
 
             this.offset.Y = offset;
